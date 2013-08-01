6df5c9cd96a067400cb8bbc85deade7e8d20f70b
tghenov <hovingart@gmail.com>
2013-08-01 20:26:22 +0300

    HelloWorld

---
+++ 01.HelloCSharp/HelloCSHarp.cs
@@ -0,0 +1,9 @@
+﻿//Create, compile and run a "Hello C#" console application
+using System;
+class HelloCSHarp
+{
+    static void Main()
+    {
+        System.Console.WriteLine("Hello C#");
+    }
+}
